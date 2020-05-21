55bb98b9d4aa7dc33eb6e42cdba39abe7f64f804
root <root@nextcloudhub.lan>
2020-05-21 10:08:15 -0400

    scan data files

--- scan_files.sh
+++ scan_files.sh
@@ -5,9 +5,9 @@ echo "Scanning data files for nextcloud users"
 echo "#######################################"
 echo
 echo "Finding files not in www-data group..."
-find /mnt/owncloud-data/misu/files ! -group www-data -print -exec chown www-data:www-data {} \;
+find /mnt/owncloud-data/ ! -group www-data -print -exec chown www-data:www-data {} \;
 echo "Owner and group check done. "
 echo "Next scanning all files now..."
 echo
-sudo -u www-data php /var/www/nextcloud/occ files:scan misu
+sudo -u www-data php /var/www/nextcloud/occ files:scan --all
 exit
