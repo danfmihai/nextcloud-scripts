2b4312cdf6e6b7852d365b633afb4bf365415456
root <root@nextcloudhub.lan>
2020-05-21 07:17:50 -0400

    Backup Nextcloud

--- backup-nextcloud.sh
+++ backup-nextcloud.sh
@@ -3,10 +3,9 @@
 # Replace all working directories with yours 
 ## MAKE SURE YOU ARE RUNNING THE SCRIPT AS SUDO
 # Working folders #
+
+## REPLACE YOUR NEXTCLOUD FOLDER NAME BELOW WITH YOURS IF NOT THE SAME NAME:
 nextcloud_folder_name=nextcloud              # Nextcloud installation folder name
-# db_name='nextcloud'                           # Database name
-# db_user='root'
-# db_pass=''
 dir=/var/www                                 #nginx or apache root folder 
 nextcloud_dir=$dir/$nextcloud_folder_name    #nextcloud installation folder variable
 backup_dir=~/nextcloud_backup                #backup base dir
@@ -14,14 +13,17 @@ backup_remote=root@proxmox.lan               #backup remote host whre to transfe
 backup_port=30004                            #backup remote host port
 backup_location=/mnt/backup/vps/nextcloud/   #backup remote destination for the backup files
 backup_log=~/backup_$(date '+%m-%d-%Y-%H:%M').log
-
+user_cred=$backup_dir/user_cred.txt
 clear
 
 get_info () {
     version=$(cat $nextcloud_dir/config/config.php | grep version | awk '{print $3}' | sed "s/['\,,\"]//g")
     _dbname=$(cat $nextcloud_dir/config/config.php | grep dbname | awk '{print $3}' | sed "s/['\,,\"]//g")
     _dbuser=$(cat $nextcloud_dir/config/config.php | grep dbuser | awk '{print $3}' | sed "s/['\,,\"]//g")
-    _dbpass=$(cat $nextcloud_dir/config/config.php | grep dbpassord | awk '{print $3}' | sed "s/['\,,\"]//g")
+    _dbpass=$(cat $nextcloud_dir/config/config.php | grep dbpassword | awk '{print $3}' | sed "s/['\,,\"]//g")
+    echo "[client]" > $user_cred
+    echo "user='${_dbuser}'" >> $user_cred
+    echo "password='${_dbpass}'" >> $user_cred
 }
 
 ##### BACKUP OF NEXTCLOUD INSTALLATION #####
@@ -31,40 +33,49 @@ echo
 
       if [ -d  $nextcloud_dir ]
       then
+            
+            # test if database folder exists
+            if [ ! -d $backup_dir/database ]
+            then
+                  echo "Backup folder doesn't exists on your filesystem."
+                  mkdir -p $backup_dir/database
+                  if [ -d $backup_dir/database ] 
+                  then  echo "Folder ${backup_dir}/database created.."
+                     cd $backup_dir/database
+                  else echo "Backup folder could not be created - ERROR!"
+                  fi
+            fi      
+            
             # get the version of nextcloud install
             get_info
             echo "*******************************"
             echo "Database user found: ${_dbuser}"
             echo "Database name found: ${_dbname}"
             echo "*******************************"            
-            #version=$(cat $nextcloud_dir/config/config.php | grep version | awk '{print $3}' | sed "s/['\,,\"]//g")
+            echo
+            
+            # name of the database file that will be created as backup
             db_file=nextcloud_$( date '+%m-%d-%Y' )_$version.sql
+            # tar file of nextcloud folder
             tar_file=nextcloud_$( date '+%m-%d-%Y' )_$version.tar.gz
+
             echo "Any existing backup files ${db_file} and ${tar_file} will be overwritten!"
+            echo
             echo "Backing up NextCloud version ${version} database...Please wait..."
-            # test if database folder exists
-            if [ ! -d $backup_dir/database ]
-            then
-                  echo "database folder doesn't exists on your filesystem."
-                  mkdir -p $backup_dir/database
-                  if [ -d $backup_dir/database ] 
-                  then  echo "folder ${backup_dir} created.."
-                     cd $backup_dir/database
-                  else echo "folder database not created - ERROR!"
-                  fi
-            fi      
+            echo            
             cd $backup_dir
             echo "Saving the database nextcloud to ${backup_dir}/database/${db_file}"
             umask 177
-            mysqldump -u $_dbuser -p$db_pass $db_name > $backup_dir/database/$db_file
+            #mysqldump -u $_dbuser -p$_dbpass $_dbname > $backup_dir/database/$db_file
+            mysqldump --defaults-extra-file=$user_cred $_dbname > $backup_dir/database/$db_file
             echo "done."
             echo
-            echo "CREATING BACKUP OF ${nextcloud_dir} FOLDER - as tar file..."
+            echo "CREATING BACKUP OF ## ${nextcloud_dir} ## FOLDER - as tar file..."
             echo "Please wait..."
             cd $dir 
             # Creating archive tar for folder nextcloud/
             tar cpzf $backup_dir/$tar_file nextcloud/
-            echo "Tar archive done. ${tar_file}"
+            echo "Tar ${tar_file} archive done. "
             echo
             echo "Transfering the backup file and the database... "
             echo "Please wait..."
